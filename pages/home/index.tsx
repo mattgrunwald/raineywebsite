2b0c7ce00cef6ef1f68c78c33f1394d67ae06bfe
Matt Grunwald <matt.grunwald.dev@gmail.com>
2022-10-03 14:04:48 -0400

    add fade in

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,23 +1,36 @@
 import type { NextPage } from 'next'
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 
 import Circles from './Circles'
 import Text from './Text'
 
 const Home: NextPage = () => {
     return (
-    <Container>
+    <Containers>
         <Circles/>
         <Text />
-    </Container>
+    </Containers>
     )
 }
 
-const Container = styled.div`
+const fadeIn = keyframes`
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+`
+
+const Containers = styled.div`
     display: flex;   
     flex-direction: row;
     justify-content: center;
     align-items: center;
+    animation: ${fadeIn} 1s linear;
+    transition: visibility 1s linear;
 `
+
+
 
 export default Home
