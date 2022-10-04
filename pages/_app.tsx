fd7b79424e6528d74ed33098216d6720b72d151b
Matt Grunwald <matt.grunwald.dev@gmail.com>
2022-10-03 20:17:48 -0400

    make styles load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import NavBar from '../util/navbar'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="Rainey Grunwald" content="Rainey Grunwald Portfolio"/>
+      </Head>
       <NavBar />
       <Component {...pageProps} />
     </>
