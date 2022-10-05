a3e2e181bf64b39832cc82db13cf856de8de411e
Matt Grunwald <matt.grunwald.dev@gmail.com>
2022-10-04 23:19:51 -0400

    slide transition

--- util/navbar/Burger.tsx
+++ util/navbar/Burger.tsx
@@ -1,4 +1,4 @@
-import styled, {keyframes} from 'styled-components'
+import styled, {keyframes, css} from 'styled-components'
 import Hamburger from 'hamburger-react'
 
 import { computerHide } from '../shared'
@@ -12,18 +12,24 @@ export default function Burger() {
         setOpen(false)
     }
 
-    function handleChange(state: any) {
-        setOpen(state.isOpen)
-    }
-
     function handleClick() {
         setOpen(!isOpen)
     }
+
     return (
         <BurgerContainer>
             <Hamburger toggled={isOpen} toggle={handleClick}/>
-            { isOpen && 
-                <Menu>
+                <div style={{
+                    width: '250px',
+                    position: 'fixed',
+                    height: '100vh',
+                    left: '-300px',
+                    top: '0',
+                    backgroundColor: 'var(--mint-green)',
+                    padding: '2.5em 1.5em 0',
+                    transform: isOpen?  'translate3d(300px, 0, 0)': '',
+                    transition: 'all 0.5s',
+                }}>
                     <Item>
                         <Link id="home" href="/home"><A onClick={close}>Home</A></Link>
                     </Item>
@@ -33,9 +39,7 @@ export default function Burger() {
                     <Item>
                         <Link id="contact" href="/contact"><A onClick={close}>Contact</A></Link>
                     </Item>
-                </Menu>
-            
-            }
+                </div>
         </BurgerContainer>
     )
 }
@@ -52,27 +56,4 @@ const A = styled.span`
     color: white;
     font-size: 24px;
     cursor: pointer;
-`
-
-const fadeIn = keyframes`
-    0% {
-        opacity: 0;
-    }
-    100% {
-        opacity: 1;
-    }
-`
-
-const Menu = styled.div`
-    width: 250px;
-    height: 100vh;
-    position: fixed;
-    left: 0;
-    top: 0;
-    background-color: var(--mint-green);
-    padding: 2.5em 1.5em 0;
-    fontSize: 1.15em;
-    animation: ${fadeIn} .25s linear;
-    transition: 10s;
-    left: 0;
 `
